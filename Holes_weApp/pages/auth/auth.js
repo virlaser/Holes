cd9533246c3d41a4f6332614e3ba78b287747f38
virlaser <liziyan784617756@foxmail.com>
2018-05-11 17:48:24 +0800

    [新增] 新增用户授权界面
    使用单独界面进行用户授权，避免因api过时引起授权失败

---
+++ Holes_weApp/pages/auth/auth.js
@@ -0,0 +1,27 @@
+//js
+Page({
+  data: {
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  onLoad: function () {
+    // 查看是否授权
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              console(res.userInfo)
+            }
+          })
+        }
+      }
+    })
+  },
+  bindGetUserInfo: function (e) {
+    console.log(e.detail.userInfo)
+    wx.reLaunch({
+      url: '/pages/index/index',
+    })
+  }
+})
