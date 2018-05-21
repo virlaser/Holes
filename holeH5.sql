35b68b848806a67e969286ed844f0eb6c21f45b0
virlaser <liziyan784617756@foxmail.com>
2018-05-21 15:27:31 +0800

    [新增] 新增树洞新数据库以及树洞新的h5
    微信小程序上线失败，不具备运营论坛类小程序的资质
    转h5继续开发，不过少了openID之类的方便的认证功能
    在用户登录认证方面要做出很大的妥协

---
+++ holeH5.sql
@@ -0,0 +1,92 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MAMP
+ Source Server Type    : MySQL
+ Source Server Version : 50638
+ Source Host           : localhost:3306
+ Source Schema         : holeH5
+
+ Target Server Type    : MySQL
+ Target Server Version : 50638
+ File Encoding         : 65001
+
+ Date: 21/05/2018 15:21:03
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for hole_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `hole_comment`;
+CREATE TABLE `hole_comment` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `content` varchar(800) NOT NULL DEFAULT '' COMMENT '用户发送评论内容',
+  `like_num` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '评论点赞数',
+  `content_id` int(10) unsigned NOT NULL COMMENT '帖子id',
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '发送评论用户的id',
+  `hide` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否匿名，0：匿名，1：实名',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for hole_content
+-- ----------------------------
+DROP TABLE IF EXISTS `hole_content`;
+CREATE TABLE `hole_content` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `content` varchar(800) NOT NULL DEFAULT '' COMMENT '用户发送树洞内容',
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '发送内容用户的id，用户可以完全匿名发送',
+  `tag` varchar(50) NOT NULL DEFAULT '' COMMENT '发送内容的标签',
+  `flag` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '1:置顶',
+  `hide` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否匿名，0：匿名，1：实名',
+  `like_num` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '点赞人数',
+  `dislike_num` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '点踩人数',
+  `comment_num` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '评论人数',
+  `report_num` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '举报人数',
+  `is_delete` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '用户是否删除了帖子，0：未删除，1：删除',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for hole_operate
+-- ----------------------------
+DROP TABLE IF EXISTS `hole_operate`;
+CREATE TABLE `hole_operate` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '用户操作类型：1.点赞，2.点踩，3.评论，4.举报，5.给评论点赞',
+  `identity` varchar(50) NOT NULL DEFAULT '0' COMMENT '操作用户标识，没有登录的情况下点赞识别',
+  `from_user` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '操作用户的id',
+  `to_user` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '要通知用户的id',
+  `flag` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '用户是否查看操作通知，0：未查看，1：查看',
+  `object_id` int(10) unsigned NOT NULL COMMENT '用户操作对象id，type=5时为评论id，其余为内容id',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for hole_user
+-- ----------------------------
+DROP TABLE IF EXISTS `hole_user`;
+CREATE TABLE `hole_user` (
+  `identity` varchar(50) NOT NULL DEFAULT '' COMMENT '用户加密标识',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(10) NOT NULL DEFAULT '' COMMENT '用户昵称',
+  `avatar` varchar(300) NOT NULL DEFAULT '' COMMENT '用户头像',
+  `gender` tinyint(1) NOT NULL DEFAULT '0' COMMENT '用户性别，0：未知，1：男，2：女',
+  `mail` varchar(50) NOT NULL DEFAULT '' COMMENT '用户的邮箱',
+  `password` varchar(50) NOT NULL DEFAULT '' COMMENT '用户密码',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`identity`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ think-5.0.19/application/hole/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vlaser
+ * Date: 2018/5/21
+ * Time: 下午3:25
+ */
+
+namespace app\hole\controller;
+
+
+class Index {
+
+    public function index() {
+        return '欢迎来到武理树洞H5';
+    }
+}
