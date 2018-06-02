38217ad720e13ea086bb2b87d631ae51730c4a7a
virlaser <liziyan784617756@foxmail.com>
2018-06-02 12:44:11 +0800

    [完善] 用户登录与注册之间的页面跳转
    1.登录与注册的页面跳转逻辑
    2.注册用户的判断
    3.错误信息的展示

--- think-5.0.19/application/hole/controller/User.php
+++ think-5.0.19/application/hole/controller/User.php
@@ -10,11 +10,19 @@ namespace app\hole\controller;
 
 
 use think\Controller;
+use think\Db;
+use think\Request;
+use app\hole\common;
 
 class User extends Controller {
 
-    public function index() {
-        return $this->fetch();
+    public function index(Request $request) {
+        $isLogin = common\isLogin($request);
+        if($isLogin['type'] == 'userV' and $isLogin['status'] == 'success') {
+            return $this->fetch();
+        } else {
+            return $this->fetch('login');
+        }
     }
 
     public function my() {
@@ -33,8 +41,39 @@ class User extends Controller {
         return $this->fetch();
     }
 
+    public function doLogin(Request $request) {
+        $email = $request->param('userMail');
+        $password = $request->param('userPassword');
+        return $email.$password;
+    }
+
     public function register() {
         return $this->fetch();
+    }
+
+    public function doRegister(Request $request) {
+        $userName = $request->param('userName');
+        $userMail = $request->param('userMail');
+        $userPassword = $request->param('password');
+        $isRegisted = Db::name('user')
+            ->where('mail', '=', $userMail)
+            ->find();
+        if($isRegisted) {
+            $errorMessage = "此邮箱已经被注册";
+            $this->assign('errorMessage', $errorMessage);
+            return $this->fetch('message/error');
+        } else {
+            Db::name('user')
+                ->insert([
+                    'nickname' => $userName,
+                    'mail' => $userMail,
+                    'password' => md5($userPassword)
+                ]);
+            $errorMessage = "注册成功";
+            $this->assign('errorMessage', $errorMessage);
+            return $this->fetch('message/error');
+        }
+
     }
 
 }
