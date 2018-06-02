130062d58016c17cca1c552056a1ce02b01240f3
virlaser <liziyan784617756@foxmail.com>
2018-06-02 12:41:12 +0800

    [完善] 完善用户注册逻辑
    1.用户注册页面
    2.用户注册时的JavaScript字段校验
    3.用户注册的Controller逻辑
    4.用户注册成功与否的提示

---
+++ think-5.0.19/public/static/js/index.js
@@ -0,0 +1,34 @@
+function doRegister() {
+    let userName = $('#name').val();
+    let userMail = $('#mail').val();
+    let pwd1 = $('#pwd1').val();
+    let pwd2 = $('#pwd2').val();
+
+    if(!userName) {
+        alert("请填写用户名");
+        return false;
+    }
+
+    let mailReg = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/;
+    console.log(userMail);
+    if(!mailReg.test(userMail)) {
+        alert("请填写正确邮箱");
+        return false;
+    }
+
+    if(!pwd1 || !pwd2) {
+        alert("请填写密码");
+        return false;
+    } else {
+        if(pwd1 === pwd2) {
+            if(pwd1.length < 6) {
+                alert("请填写六位以上密码");
+                return false;
+            }
+        } else {
+            alert("两次填写密码不一致");
+            return false;
+        }
+    }
+
+}
