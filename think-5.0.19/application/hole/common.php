282be5eb3d1c4d2eb432d20685050f5a6890c362
virlaser <liziyan784617756@foxmail.com>
2018-06-02 12:42:42 +0800

    [新增] 公共函数文件
    1.判断用户是否注册
    2.设置登录用户与未登录用户的cookie

---
+++ think-5.0.19/application/hole/common.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vlaser
+ * Date: 2018/6/1
+ * Time: 下午5:58
+ */
+
+namespace app\hole\common;
+
+use think\Cookie;
+use think\Db;
+use think\Request;
+
+function isLogin(Request $request) {
+    $data = array();
+    $userT = $request->cookie('useT');
+    $userV = $request->cookie('userV');
+    if($userV) {
+        $user = Db::name('user')
+            ->where('identity', '=', $userV)
+            ->field('id')
+            ->find();
+        if($user) {
+           $data = [
+               'message' => '用户已登录',
+               'type' => 'userV',
+               'user' => $user['id'],
+               'status' => 'success'
+           ];
+           return $data;
+        } else {
+            $data = [
+                'message' => '登录认证信息错误',
+                'type' => 'userV',
+                'user' => null,
+                'status' => 'fail'
+            ];
+            return $data;
+        }
+    } elseif ($userT) {
+        $data = [
+            'message' => '用户未登录',
+            'type' => 'userT',
+            'user' => $userT,
+            'status' => 'success'
+        ];
+        return $data;
+    } else {
+        $data = [
+            'message' => '无用户信息',
+            'type' => 'userT',
+            'user' => null,
+            'status' => 'fail'
+        ];
+        return $data;
+    }
+}
+
+// hash time
+function setUserT() {
+    $microTime = explode(" ", microtime());
+    $time = $microTime[1] . ($microTime[0] * 1000);
+    $time = explode(".", $time)[0];
+    $userT = md5($time);
+    Cookie::set('userT', $userT, ['prefix' => 'hole_', 'expire' => 60*60*24*365]);
+}
+
+// hash username and password
+function setUserV($userId) {
+    $identity = Db::name('user')
+        ->where('id', '=', $userId)
+        ->field('identity')
+        ->find();
+    Cookie::set('userV', $identity['identity'], ['prefix' => 'hole_', 'expire' => 60*60*24*30]);
+}
+
+function sendMail($emailAddress) {
+    $data = [
+        'message' => '邮件发送成功',
+        'status' => 'success'
+    ];
+    return $data;
+}
