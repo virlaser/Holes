717eb61984eaf9cb47f115cee9efa5d0bff5f876
virlaser <liziyan784617756@foxmail.com>
2018-06-15 15:19:31 +0800

    [完善] 完善了前端点赞和点踩ajax的逻辑
    在dom元素中全部使用onlick来监听用户点击逻辑
    而不是采用<a>标签

--- think-5.0.19/public/static/js/index.js
+++ think-5.0.19/public/static/js/index.js
@@ -57,3 +57,70 @@ function doCreate() {
         return false;
     }
 }
+
+function doVote(event, contentId, type) {
+    let img = $(event).find('img');
+    let p = $(event).find('p');
+    let staticRes = "/static/images/";
+
+    $.ajax({
+        type: 'POST',
+        url: '/operate',
+        data: {
+            'contentId': contentId,
+            'type': type
+        },
+        dataType: 'json',
+        timeout: 300,
+        success: function () {
+            if(type === 1) {
+                let src = img.attr('src').slice(-6, -4);
+                if(src === 'ed') {
+                    img.attr('src', staticRes+'icon-like.png');
+                    if(parseInt(p.html()) === 1) {
+                        p.html('赞');
+                    } else {
+                        p.html(parseInt(p.html())-1);
+                    }
+                } else {
+                    img.attr('src', staticRes+'icon-like-selected.png');
+                    if(p.html() === '赞') {
+                        p.html('1');
+                    } else {
+                        p.html(parseInt(p.html())+1);
+                    }
+                }
+            }
+            if(type === 2) {
+                let src = img.attr('src').slice(-6, -4);
+                if(src === 'ed') {
+                    img.attr('src', staticRes+'icon-dislike.png');
+                    if(parseInt(p.html()) === 1) {
+                        p.html('踩');
+                    } else {
+                        p.html(parseInt(p.html())-1);
+                    }
+                } else {
+                    img.attr('src', staticRes+'icon-dislike-selected.png');
+                    if(p.html() === '踩') {
+                        p.html('1');
+                    } else {
+                        p.html(parseInt(p.html())+1);
+                    }
+                }
+            }
+        },
+        error: function () {
+            alert("网络错误");
+        }
+    })
+}
+
+function doComment(contentId) {
+
+}
+
+function getDetail(event, contentId) {
+    console.log('click getDetail');
+    window.location.href = '/detail?contentId=' + contentId;
+}
