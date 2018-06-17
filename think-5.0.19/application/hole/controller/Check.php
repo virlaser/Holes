dcda2aca3fa3e9949eddd7f0a3e69290a4ccbe39
virlaser <liziyan784617756@foxmail.com>
2018-06-17 20:05:43 +0800

    [完善] 后端审帖逻辑
    1.用户登录状态判断
    2.按照时间顺序选取用户为审阅的帖子

--- think-5.0.19/application/hole/controller/Check.php
+++ think-5.0.19/application/hole/controller/Check.php
@@ -10,11 +10,73 @@ namespace app\hole\controller;
 
 
 use think\Controller;
+use think\Db;
+use think\Request;
+
+use app\hole\common;
 
 class Check extends Controller {
 
-    public function index() {
+    public function index(Request $request) {
+        common\setUserT($request);
+        $isLogin = common\isLogin($request);
+        $userT = $isLogin['type']=='userT'?$isLogin['user']:0;
+        $checked = Db::name('operate')
+            ->where([
+                'type' => 7,
+                $isLogin['type']=='userV'?'from_user':'identity' => $isLogin['user']
+            ])
+            ->field('object_id')
+            ->select();
+        $checkArray = array();
+        foreach ($checked as $item) {
+            array_push($checkArray, $item['object_id']);
+        }
+        $content = Db::name('content')
+            ->where([
+                'userV' => 0,
+                'userT' => ['NEQ', $userT],
+                'verified' => ['<', 4],
+                'id' => ['NOT IN', $checkArray]
+            ])
+            ->find();
+        $this->assign('content', $content);
         return $this->fetch();
+    }
+
+    public function doCheck(Request $request) {
+        common\setUserT($request);
+        $isLogin = common\isLogin($request);
+        $contentId = $request->param('contentId');
+        $checkType = $request->param('checkType');
+        // todo xss
+        Db::name('operate')
+            ->insert([
+                'type' => 7,
+                $isLogin['type']=='userV'?'from_user':'identity' => $isLogin['user'],
+                'to_user' => 0,
+                'flag' => 0,
+                'object_id' => $contentId
+            ]);
+        if($checkType == 'yes') {
+            Db::name('content')
+                ->where([
+                    'id' => $contentId,
+                ])
+                ->setInc('verified');
+        }
+        if($checkType == 'no') {
+            Db::name('content')
+                ->where([
+                    'id' => $contentId,
+                ])
+                ->setDec('verified');
+        }
+        $data = [
+            'status' => 'success',
+            'message' => '审帖成功'
+        ];
+        return json($data);
     }
 
 }
