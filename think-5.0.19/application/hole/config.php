af7be0ca4bbfdbe9a8976bd9aee8be193e758b66
virlaser <liziyan784617756@foxmail.com>
2018-06-18 14:42:34 +0800

    [新增] 新增了发送邮件的163邮箱配置

---
+++ think-5.0.19/application/hole/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vlaser
+ * Date: 2018/6/18
+ * Time: 下午1:05
+ */
+
+return [
+    'mail' => [
+        'host' => 'smtp.163.com',
+        'port' => 465,
+        'userName' => '15871399329@163.com',
+        'password' => 'Lzy784617756',
+        'from' => '15871399329@163.com'
+    ]
+];
