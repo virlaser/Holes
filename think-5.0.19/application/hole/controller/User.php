d63242c39337f6d9d88105aaf10a284fb5c54c18
virlaser <liziyan784617756@foxmail.com>
2018-06-20 17:59:08 +0800

    [新增] 图片上传 init
    前端图片预览
    上传后后端图片压缩

--- think-5.0.19/application/hole/controller/User.php
+++ think-5.0.19/application/hole/controller/User.php
@@ -718,4 +718,35 @@ class User extends Controller {
             return $this->fetch('message/error');
         }
     }
+
+    public function upload() {
+        return $this->fetch();
+    }
+
+    public function doUpload(Request $request) {
+        $img = isset($_POST['img'])? $_POST['img'] : '';
+
+        // 获取图片
+        list($type, $data) = explode(',', $img);
+
+        // 判断类型
+        if(strstr($type,'image/jpeg')!=''){
+            $ext = '.jpg';
+        }elseif(strstr($type,'image/gif')!=''){
+            $ext = '.gif';
+        }elseif(strstr($type,'image/png')!=''){
+            $ext = '.png';
+        }
+
+        // 生成的文件名
+        $photo = time().$ext;
+
+        // 生成文件
+        file_put_contents($photo, base64_decode($data), true);
+
+        // 返回
+        header('content-type:application/json;charset=utf-8');
+        $ret = array('img'=>$photo);
+        return json_encode($ret);
+    }
 }
