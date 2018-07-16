c6ce1503111529499fd0a7b15b826039125b3a4d
virlaser <liziyan784617756@foxmail.com>
2018-07-17 00:34:21 +0800

    [修改] 管理页面DOM修改，管理页面ajax操作逻辑

--- think-5.0.19/public/static/js/admin.js
+++ think-5.0.19/public/static/js/admin.js
@@ -9,13 +9,85 @@ function doAdminLogin() {
 }
 
 function doDelete(contentId) {
-
+    let confirm = this.confirm("确定删除这条帖子？");
+    if(confirm === true) {
+        $.ajax({
+           type: 'POST',
+           url: '/doDelete',
+           data: {
+               'contentId': contentId
+           },
+           dataType: 'json',
+           timeout: 5000,
+           success: function (res) {
+              if(res.status === 'success') {
+                  alert(res.message);
+                  location.reload();
+              }
+           }
+        })
+    }
 }
 
-function doBan(contentId) {
-
+function doBan(userId) {
+    let confirm = this.confirm("确定封禁这个用户？");
+    if(confirm === true) {
+        $.ajax({
+            type: 'POST',
+            url: '/doBan',
+            data: {
+                'userId': userId
+            },
+            dataType: 'json',
+            timeout: 5000,
+            success: function (res) {
+                if(res.status === 'success') {
+                    alert(res.message);
+                    location.reload();
+                }
+            }
+        })
+    }
 }
 
 function doTop(contentId) {
+    let confirm = this.confirm("确定置顶这条帖子？");
+    if(confirm === true) {
+        $.ajax({
+            type: 'POST',
+            url: '/doTop',
+            data: {
+                'contentId': contentId
+            },
+            dataType: 'json',
+            timeout: 5000,
+            success: function (res) {
+                if(res.status === 'success') {
+                    alert(res.message);
+                    location.reload();
+                }
+            }
+        })
+    }
+}
 
+function doDeleteTag(tag) {
+    let confirm = this.confirm("确定删除这个标签？所有帖子这个标签将被删除！");
+    if(confirm === true) {
+        $.ajax({
+            type: 'POST',
+            url: '/doDeleteTag',
+            data: {
+                'tag': tag
+            },
+            dataType: 'json',
+            timeout: 5000,
+            success: function (res) {
+                if(res.status === 'success') {
+                    alert(res.message);
+                    location.reload();
+                }
+            }
+        })
+    }
 }
